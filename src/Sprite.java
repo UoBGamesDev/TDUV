2d2c1c47e9627771bde69a8525f8e8f1d99d71a1
Joel Hoskin <joelhoskin@outlook.com>
2013-05-07 15:36:20 +0100

    Made rendering pay attention to entity width and height

--- src/Sprite.java
+++ src/Sprite.java
@@ -22,7 +22,7 @@ public class Sprite
 	@Override
 	public void render(Rectangle position)
 	{
-		image.draw(position.x, position.y);
+		image.draw(position.x, position.y, position.width, position.height);
 	}
 
 }
